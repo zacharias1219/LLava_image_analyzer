6f11a61bd5f4c85aa3ae643008a71796235ab536
Richard Abishai <rickoshade1891@gmail.com>
2024-04-29 19:09:46 +0530

    config

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    PAGE_TITLE = "LLava Image Analyzer"
+
+    OLLAMA_MODELS = ('llava:13b', 'llava:34b', 'bakllava')
+
+    SYSTEM_PROMPT = f"""You are a helpful chatbot that has access to the following 
+                    open-source vision models {OLLAMA_MODELS}.
+                    You can can answer questions about images."""
